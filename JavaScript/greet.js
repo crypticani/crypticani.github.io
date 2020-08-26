0a73372eed9152242128f441d9a18c2a2f65182a
Aniket Kumar <44335648+crypticani@users.noreply.github.com>
2020-08-26 11:05:41 +0530

    Add files via upload

---
+++ JavaScript/greet.js
@@ -0,0 +1,11 @@
+var now = new Date();
+var hrs = now.getHours();
+var msg = "";
+
+if (hrs >  0) msg = "Mornin' Sunshine!"; // REALLY early
+if (hrs >  4) msg = "Good Morning!";      // After 6am
+if (hrs > 12) msg = "Good Afternoon!";    // After 12pm
+if (hrs > 17) msg = "Good Evening!";      // After 5pm
+if (hrs > 23) msg = "Go to bed!";        // After 10pm
+
+document.write(msg+" "+" Welcome to my website.");
