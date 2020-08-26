76c8a4463160b314736c86a0ae22fe6740d74c34
Aniket Kumar <44335648+crypticani@users.noreply.github.com>
2020-08-26 12:22:01 +0530

    Fixed time issue

--- JavaScript/greet.js
+++ JavaScript/greet.js
@@ -1,11 +1,9 @@
-var now = new Date();
-var hrs = now.getHours();
+var myDate = new Date();
+var  hrs = myDate.getHours();
 var msg = "";
 
-if (hrs >  0) msg = "Mornin' Sunshine!"; // REALLY early
-if (hrs >  4) msg = "Good Morning!";      // After 6am
-if (hrs > 12) msg = "Good Afternoon!";    // After 12pm
-if (hrs > 17) msg = "Good Evening!";      // After 5pm
-if (hrs > 23) msg = "Go to bed!";        // After 10pm
+if (hrs<12) msg = "Good Morning!";      // After 6am
+if (hrs >= 12 && hrs<17) msg = "Good Afternoon!";    // After 12pm
+if (hrs >= 17 && hrs<=24) msg = "Good Evening!";      // After 5pm
 
 document.write(msg+" "+" Welcome to my website.");
