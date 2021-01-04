3151ac4fe990d6cc6991b08bfab506829321d9df
Aniket Kumar <44335648+crypticani@users.noreply.github.com>
2021-01-04 17:25:00 +0530

    Update greet.js

--- assets/js/greet.js
+++ assets/js/greet.js
@@ -6,4 +6,4 @@ if (hrs<12) msg = "Good Morning!";      // After 6am
 if (hrs >= 12 && hrs<17) msg = "Good Afternoon!";    // After 12pm
 if (hrs >= 17 && hrs<=24) msg = "Good Evening!";      // After 5pm
 
-document.write("Hi there! " + msg);
+document.write("Hi there, " + msg);
